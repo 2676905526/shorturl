e02a0255ca886ded331519fba27b1fa2c2f25200
ztyangt <yang2210670@163.com>
2022-09-12 18:06:31 +0800

    修正https

--- routes/api.py
+++ routes/api.py
@@ -2,7 +2,7 @@ import re,time,urllib,random
 from unittest import result
 from flask import Blueprint,jsonify, request
 from utils import SqlHelper,s64,getSoup
-
+from urllib.parse import urlparse
 
 api = Blueprint('api', __name__)
 
@@ -10,6 +10,7 @@ api = Blueprint('api', __name__)
 @api.route('/', methods=['GET'])
 def loadmore():
     # url = request.values.get("url")
+    host = f"{urlparse(request.host_url).scheme}s://{urlparse(request.host_url).netloc}/"
     url = urllib.parse.unquote(request.url.split("url=")[-1])
     if url == request.url:
         return jsonify({"code": 400, "msg": "请输入URL"})
@@ -21,10 +22,10 @@ def loadmore():
         longURL = result[0]
         urlQuery = shortDB.table('shortURL').where(f"longUrl = '{longURL}'").find()
         if len(urlQuery) > 0:
-            data = {'shortID': urlQuery[0]["shortID"], 'longUrl':  urlQuery[0]["longUrl"],"shortUrl": f'{request.host_url}{urlQuery[0]["shortID"]}','creatTime':  urlQuery[0]["creatTime"]}
+            data = {'shortID': urlQuery[0]["shortID"], 'longUrl':  urlQuery[0]["longUrl"],"shortUrl": f'{host}{urlQuery[0]["shortID"]}','creatTime':  urlQuery[0]["creatTime"]}
             return jsonify({"code": 200, "msg": "URL已存在","data": data })
         shortID = s64(int(time.time()))
-        data = {'shortID': shortID, 'longUrl': longURL,"shortUrl":f'{request.host_url}{shortID}','creatTime': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}
+        data = {'shortID': shortID, 'longUrl': longURL,"shortUrl":f'{host}{shortID}','creatTime': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}
         shortDB.table("shortURL").add(data)
         shortDB.close()
         return jsonify({
